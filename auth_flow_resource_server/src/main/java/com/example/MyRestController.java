cea449f0d13acfe3f86dee7f49c811e6e2a35093
Lance Fallon <Lance Fallon>
2016-11-13 13:26:23 -0500

    additional endpoint

--- auth_flow_resource_server/src/main/java/com/example/MyRestController.java
+++ auth_flow_resource_server/src/main/java/com/example/MyRestController.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -16,4 +19,8 @@ public class MyRestController {
 		return new ResponseEntity<>(auth, HttpStatus.OK);
 	}
 
+	@RequestMapping(value="/system", method = RequestMethod.GET)
+	public ResponseEntity<InetAddress> getSystemInfo(OAuth2Authentication auth) throws UnknownHostException {
+		return new ResponseEntity<>(InetAddress.getLocalHost(), HttpStatus.OK);
+	}
 }
